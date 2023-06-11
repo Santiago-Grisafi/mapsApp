0f5548e03b13ec35b51909b2a04a0323c652397d
Santiago <sngrisa@gmail.com>
2023-06-11 16:33:41 -0300

    Fix Img

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 
@@ -10,12 +9,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent,
     SidebarComponent
   ],
   exports:[
     NavbarComponent,
-    FooterComponent,
     SidebarComponent
   ],
   imports: [
